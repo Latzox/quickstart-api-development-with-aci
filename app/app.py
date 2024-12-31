91686558f34b6d67ec57d86645ea70b39deb9de3
Marco Platzer <latzox@outlook.com>
2024-12-31 18:35:14 -0500

    Update app

--- app/app.py
+++ app/app.py
@@ -4,10 +4,14 @@ import os
 app = Flask(__name__)
 
 container_id = os.getenv("HOSTNAME", "Unknown Container")
+client_id = os.getenv("CLIENT_ID", "Unknown Client ID")
 
 @app.route("/")
 def home():
-    return f"Hello, Community! This is a Python container app served by container ID: {container_id}"
+    return (
+        f"Hello, Community! This is a Python container app served by container ID: {container_id}. "
+        f"The CLIENT_ID is: {client_id}"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
